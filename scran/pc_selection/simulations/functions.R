84502c087642093877b91b79b2a31c23e2681004
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-30 17:49:26 +0100

    Report MSE properly, get optimal number of PCs.

--- scran/pc_selection/simulations/functions.R
+++ scran/pc_selection/simulations/functions.R
@@ -8,7 +8,7 @@ assessAccuracy <- function(n, svd.out, center, truth)
 {
     out <- svd.out$u[,1:n] %*% (svd.out$d[1:n] * t(svd.out$v[,1:n])) 
     out <- t(out) + center
-    return(sum((out-truth)^2))
+    mean((out-truth)^2)
 }
 
 assessPCA <- function(observed, truth) 
@@ -19,33 +19,43 @@ assessPCA <- function(observed, truth)
     prog.var <- x$d^2 / (ncol(observed) - 1) 
     total.var <- sum(prog.var)
 
-    tech.var <- sum(apply(observed - truth, 1, var))
-    npcs <- length(prog.var)
-    flipped.prog.var <- rev(prog.var)
-
 ## Too unstable to fluctuations!
+#    npcs <- length(prog.var)
+#    flipped.prog.var <- rev(prog.var)
 #    estimated.contrib <- cumsum(flipped.prog.var) + flipped.prog.var * (npcs:1 - 1L)
 #    estimated.contrib <- rev(estimated.contrib)
 #    retained <- min(which(tech.var > estimated.contrib))
 
-    # Using our denoising approach, or parallel analysis.
+    # Using our denoising approach.
+    tech.var <- sum(apply(observed - truth, 1, var))
     denoised <- scran:::.get_npcs_to_keep(prog.var, tech.var)
+    
+    # Applying parallel analysis.
     approximate <- ncol(observed)>=500
     parallel <- parallelPCA(observed, BPPARAM=MulticoreParam(3), value="n", approximate=approximate, min.rank=1)
 
-    # A quick-and-dirty Marchenko-Pastur-like threshold.
-    marpast <- sum(attr(parallel, "percentVar") > colMeans(attr(parallel, "permuted.percentVar"))[1])
+    # A quick-and-dirty threshold on the upper bound on random eigenvalues.
+    top.var <- colMeans(attr(parallel, "permuted.percentVar"))[1]
+    upper <- sum(attr(parallel, "percentVar") > top.var)
 
     # Assessing each method.
     d.res <- assessAccuracy(denoised, x, center, truth)
     p.res <- assessAccuracy(parallel, x, center, truth)
-    m.res <- assessAccuracy(marpast, x, center, truth)
-    n.res <- assessAccuracy(ncol(x$u), x, center, truth)
+    u.res <- assessAccuracy(upper, x, center, truth)
+
+    # Finding the optimal number of PCs.
+    collected <- numeric(100)
+    running <- 0
+    for (i in seq_along(collected)) { 
+        running <- running + x$u[,i,drop=FALSE] %*% (x$d[i] * t(x$v[,i,drop=FALSE]))
+        collected[i] <- mean((t(running) + center - truth)^2)
+    }
+    optimal <- which.min(collected)
 
     return(list(N.denoised=denoised, MSE.denoised=d.res,
                 N.parallel=parallel, MSE.parallel=p.res,
-                N.marpast=marpast, MSE.marpast=m.res,
-                N.none=ncol(x$x), MSE.none=n.res))
+                N.upper=upper, MSE.upper=u.res,
+                N.optimal=optimal, MSE.optimal=collected[optimal]))
 }
 
 runSimulation <- function(fname, FUN) 
@@ -59,8 +69,8 @@ runSimulation <- function(fname, FUN)
 
                 collected <- vector("list", 10)
                 for (it in 1:10) {
-                    truth <- FUN(ngenes, ncells)
-                    truth[-seq_len(affected*ngenes),] <- 0
+                    truth <- FUN(ngenes*affected, ncells)
+                    truth <- rbind(truth, matrix(0, ncol=ncells, nrow=(1-affected)*ngenes))
                     y <- matrix(rnorm(ngenes*ncells, mean=truth), ncol=ncells)
                     collected[[it]] <- assessPCA(y, truth)
                 }
