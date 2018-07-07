8ecbc04251f24d72aec29bb76e821a78f430c00c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-07 17:39:28 +0100

    Added new scripts to test gene-wise variances, trajectories.

---
+++ scran/pc_selection/simulations/sim_genevar.R
@@ -0,0 +1,25 @@
+# This script generates gaussian cluster simulations with variable gene-wise variances.
+# The aim being to test whether or not Gavish and Donoho's optimal choice works when sigma is not constant.
+# It expects:
+# - NPOPS: the number of populations.
+# - SD: the standard deviation of the population means.
+
+if (!exists("NPOPS")) NPOPS <- 5
+if (!exists("SD")) SD <- 1
+
+FUN <- function(ngenes, ncells) {
+    in.pop <- sample(NPOPS, ncells, replace=TRUE)
+    pop.mean <- matrix(rnorm(NPOPS * ngenes, sd=SD), ncol=NPOPS)
+    pop.mean[,in.pop,drop=FALSE]
+}
+
+source("functions.R")
+dir.create("results", showWarnings=FALSE)
+runSimulation(sprintf("results/genevar-%s-%s.rds", NPOPS, SD), 
+    truth.FUN=FUN, 
+    iters=10,
+    observed.FUN=function(truth) {
+        sd <- rexp(nrow(truth), 1)
+        truth + rnorm(length(truth), mean=truth, sd=sd)  
+    }
+)
---
+++ scran/pc_selection/simulations/sim_trajectory.R
@@ -0,0 +1,21 @@
+# This script generates trajectories between cell populations.
+# It expects:
+# - NPOPS: the number of populations. 
+# - SD: the separation between populations.
+
+if (!exists("NPOPS")) NPOPS <- 5
+if (!exists("SD")) SD <- 1
+
+FUN <- function(ngenes, ncells) {
+    basis <- matrix(rnorm(NPOPS * ngenes, sd=SD), ncol=NPOPS)
+    p1 <- sample(NPOPS, ncells, replace=TRUE)
+    p2 <- sample(NPOPS, ncells, replace=TRUE)
+
+    src <- basis[,p1,drop=FALSE] 
+    dvec <- basis[,p2,drop=FALSE] - src
+    t(t(dvec) * runif(ncells)) + src
+}
+
+source("functions.R")
+dir.create("results", showWarnings=FALSE)
+runSimulation(sprintf("results/gaussbasis-%s-%s.txt", NPOPS, SD), FUN)
