927cf60b7953c6ca9827a49a203de2563751729b
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-07 17:39:41 +0100

    Updated SLURM submitter script.

--- scran/pc_selection/simulations/submitter.sh
+++ scran/pc_selection/simulations/submitter.sh
@@ -25,3 +25,39 @@ EOT
     done
 done
 
+# Submitting the trajectory simulations with varying parameters.
+
+for npops in 5 10 20
+do
+    for sd in 0.2 0.5 1
+    do
+sbatch << EOT
+#!/bin/bash
+#SBATCH -o logs/out-traj-${npops}-${sd}
+#SBATCH -e logs/err-traj-${npops}-${sd}
+#SBATCH -n 1
+#SBATCH --mem 8000
+
+echo 'NPOPS <- ${npops}; SD <- ${sd}; source("sim_trajectory.R")' | ${R} --slave --vanilla
+EOT
+    done
+done
+
+# Submitting the gene-wise variance simulations with varying parameters.
+
+for npops in 5 10 20
+do
+    for sd in 0.2 0.5 1
+    do
+sbatch << EOT
+#!/bin/bash
+#SBATCH -o logs/out-gene-${npops}-${sd}
+#SBATCH -e logs/err-gene-${npops}-${sd}
+#SBATCH -n 1
+#SBATCH --mem 8000
+
+echo 'NPOPS <- ${npops}; SD <- ${sd}; source("sim_genevar.R")' | ${R} --slave --vanilla
+EOT
+    done
+done
+
