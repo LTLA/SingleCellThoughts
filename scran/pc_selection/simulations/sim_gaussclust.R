418f16ed447317e2f95ff49de66761ad50bd71de
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-09 11:54:27 +0100

    Incorporated variable gene-wise variances into original scripts.

--- scran/pc_selection/simulations/sim_gaussclust.R
+++ scran/pc_selection/simulations/sim_gaussclust.R
@@ -2,9 +2,11 @@
 # It expects:
 # - NPOPS: the number of populations.
 # - SD: the standard deviation of the population means.
+# - HYPERVAR: a string specifying the variability of the technical noise across genes.
 
 if (!exists("NPOPS")) NPOPS <- 5
 if (!exists("SD")) SD <- 1
+if (!exists("HYPERVAR")) HYPERVAR <- "none"
 
 FUN <- function(ngenes, ncells) {
     in.pop <- sample(NPOPS, ncells, replace=TRUE)
@@ -14,4 +16,6 @@ FUN <- function(ngenes, ncells) {
 
 source("functions.R")
 dir.create("results", showWarnings=FALSE)
-runSimulation(sprintf("results/gaussclust-%s-%s.rds", NPOPS, SD), FUN, iters=10)
+runSimulation(sprintf("results/gaussclust-%i-%s-%s", NPOPS, SD, HYPERVAR), 
+    truth.FUN=FUN, 
+    observed.FUN=addNoise(HYPERVAR))
--- scran/pc_selection/simulations/sim_trajectory.R
+++ scran/pc_selection/simulations/sim_trajectory.R
@@ -1,10 +1,12 @@
 # This script generates trajectories between cell populations.
 # It expects:
 # - NPOPS: the number of populations. 
-# - SD: the separation between populations.
+# - SD: the standard deviation of the population means.
+# - HYPERVAR: a string specifying the variability of the technical noise across genes.
 
 if (!exists("NPOPS")) NPOPS <- 5
 if (!exists("SD")) SD <- 1
+if (!exists("HYPERVAR")) HYPERVAR <- "none"
 
 FUN <- function(ngenes, ncells) {
     basis <- matrix(rnorm(NPOPS * ngenes, sd=SD), ncol=NPOPS)
@@ -18,4 +20,6 @@ FUN <- function(ngenes, ncells) {
 
 source("functions.R")
 dir.create("results", showWarnings=FALSE)
-runSimulation(sprintf("results/trajectory-%s-%s.rds", NPOPS, SD), FUN)
+runSimulation(sprintf("results/trajectory-%i-%s-%s", NPOPS, SD, HYPERVAR), 
+    truth.FUN=FUN, 
+    observed.FUN=addNoise(HYPERVAR))
